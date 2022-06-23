5e0ddf0b7682ae24ecc926e90315537c422b4439
priyanshitaneja <priyanshi.taneja@oriserve.com>
2022-06-23 22:44:16 +0530

    modal open close functionality added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,15 @@ import "../styles/index.scss";
 import "../styles/common.scss";
 import "../styles/colors.scss";
 import "../styles/toggle.scss";
+
+const compensationBtn = document.getElementById("compensation");
+const modal = document.getElementById("modal");
+const modalCloseBtn = document.querySelector(".modal-close");
+
+compensationBtn.addEventListener("click", () => {
+  modal.classList.toggle("is-open");
+});
+
+modalCloseBtn.addEventListener("click", () => {
+  if (modal.classList.contains("is-open")) modal.classList.remove("is-open");
+});
